0cfa90f52334c0c7137633a1ae4d422278e9e269
silasoyibs <silasoyibs@gmail.com>
2024-04-19 20:27:58 +0100

    get loan process section and features section completed with all required animation

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
-"use strict";
+import { navLinkActive, navLinks, activePage } from "./navLink";
+import { showBtnAnimation } from "./navLink";
+import { btnCta } from "./navLink";
+// HomePage
 class Home {
   constructor() {
-    this.navLinkActive();
+    this.init();
   }
-  navLinkActive() {
-    const navLinks = document.querySelectorAll(".nav__link");
-    navLinks.forEach((link) => {
-      // remove active class
-      link.classList.remove("activeLink");
-      link.addEventListener("click", (e) => {
-        e.preventDefault();
-        navLinks.forEach((navLink) => {
-          navLink.classList.remove("activeLink");
-        });
-        // add activeLink class to clicked nav linked
-        link.classList.add("activeLink");
-        // get attribute of current link
-        const href = link.getAttribute("href");
-        // update link href when clicked
-        window.location.href = href;
-        if (href === window.location.href) {
-          link.classList.add("activeLink");
-        } else {
-          link.classList.remove("activeLink");
-        }
-      });
-    });
-  }
-  getSelector(className) {
-    return document.querySelector(`.+${className}`);
+  init() {
+    navLinkActive(navLinks, activePage);
+    showBtnAnimation(btnCta);
   }
 }
 document.addEventListener("DOMContentLoaded", function () {
---
+++ navLink.js
@@ -0,0 +1,26 @@
+export const navLinks = document.querySelectorAll(".nav__link");
+export const activePage = window.location.pathname.split("/")[1];
+export const btnCta = document.querySelectorAll(".btn-cta");
+console.log(activePage);
+
+// Nav Active Link
+export const navLinkActive = function (navLinks, activePage) {
+  navLinks.forEach((link) => {
+    const linkName = link.href.split("/");
+    const linkLength = linkName.length;
+    const splitedLink = linkName[linkLength - 1];
+    console.log(splitedLink);
+    if (splitedLink === activePage) {
+      link.classList.add("activeLink");
+    }
+  });
+};
+
+// Nav Button Animation
+export const showBtnAnimation = function (btnCta) {
+  btnCta.forEach((btnCta) => {
+    btnCta.addEventListener("mouseover", () => {
+      btnCta.classList.add("show-unfillanimation");
+    });
+  });
+};
