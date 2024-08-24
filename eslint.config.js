e3da9ae69153f5b2886170ec84800f8df3b6a597
silasoyibs <silasoyibs@gmail.com>
2024-08-24 12:12:01 +0100

    "dashboard ui implmented"

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+export default [
+  {
+    languageOptions: { globals: globals.browser },
+    // ignores: ["dist/*"]
+  },
+  pluginJs.configs.recommended,
+];
--- src/js/common.js
+++ src/js/common.js
@@ -124,3 +124,5 @@ export function validateEmail(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 }
+
+
